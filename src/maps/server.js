c3272eb04847b7eed5486b401cec4889aa366c35
nicolsita123 <ni.carrillo@duocuc.cl>
2024-09-10 23:31:39 -0300

    Comentarios y servidor actualizado

--- src/maps/server.js
+++ src/maps/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const sql = require('mssql');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 // Configuración de CORS
 app.use(cors());
 
+// Configuración para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configuración de la base de datos
 const config = {
   user: 'Luc_hernandez_SQLLogin_1',
   password: 'nus1f946z7',
@@ -31,6 +36,16 @@ app.get('/parcelas', async (req, res) => {
     res.status(500).send('Error al obtener parcelas de la base de datos');
   }
 });
+
+// Ruta para servir el archivo index.html
+app.get('/', (req, res) => {
+    // Ajusta la ruta para ir un nivel arriba desde `src/maps`
+    const filePath = path.join(__dirname, '..', '..', 'public', 'index.html');
+    console.log('Servidor corriendo', filePath);
+    res.sendFile(filePath);
+  });
+  
+  
 
 // Iniciar el servidor
 app.listen(port, () => {
